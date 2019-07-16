21f337c56c66bef53aaaabd91f5f4ff319d0d4c1
Shawn Edge <shawn.edge@block.one>
2019-07-16 15:36:56 -0400

    fix file path 2

--- scripts/veracode-scan.sh
+++ scripts/veracode-scan.sh
@@ -1,6 +1,4 @@
-OUTPUTDIR="/Users/travis/build/EOSIO/eosio-swift-ios-example-app"
-
 echo "starting veracode scan" && \
 java -jar vosp-api-wrappers-java-$VERACODE_WRAPPER_VERSION.jar -vid $VERACODE_API_ID -vkey $VERACODE_API_KEY \
 -action uploadandscan -appname "eosio-swift-ios-example-app" -createprofile false \
--filepath "$OUTPUTDIR/$APPNAME.app" -version "$TRAVIS_JOB_ID - $TRAVIS_JOB_NUMBER $DATE" -scantimeout 3600 
+-filepath "/Users/travis/build/EOSIO/eosio-swift-ios-example-app/$APPNAME.app" -version "$TRAVIS_JOB_ID - $TRAVIS_JOB_NUMBER $DATE" -scantimeout 3600 
